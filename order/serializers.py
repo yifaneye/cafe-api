486faeb7a220408860fa7f067cc336170e7c2206
Yifan Ai <ayfallen@hotmail.com>
2020-10-11 23:27:52 +1100

    Add more field details

--- order/serializers.py
+++ order/serializers.py
@@ -4,7 +4,7 @@ from order.models import Order, Payment, MenuItem
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    detail = serializers.CharField(initial='1 Latte', default='1 Latte', help_text='Order detail')
+    detail = serializers.CharField(initial='1 Latte with soy milk', default='1 Latte', help_text='Order detail')
     clientName = serializers.CharField(initial='John Doe', default='John Doe', help_text='Order client name')
     price = serializers.FloatField(initial='4', default='4', help_text='Order total price', read_only=True)
     isPaid = serializers.BooleanField(initial=False, default=False, help_text='Is order paid?', read_only=True)
@@ -17,12 +17,18 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class PaymentSerializer(serializers.ModelSerializer):
+    price = serializers.FloatField(initial='4', default='4', help_text='Payment price')
+    detail = serializers.CharField(initial='Cash', default='Cash', help_text='Payment detail')
+
     class Meta:
         model = Payment
         fields = '__all__'
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(initial='Latte', default='Latte', help_text='Menu Item Name')
+    price = serializers.FloatField(initial='4', default='4', help_text='Menu Item price')
+
     class Meta:
         model = MenuItem
         fields = '__all__'
