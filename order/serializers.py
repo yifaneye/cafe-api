1e9ae7d6bca18527863c8c4dab351b3ca3ebe79c
Yifan Ai <ayfallen@hotmail.com>
2020-10-11 23:13:56 +1100

    Add help text

--- order/serializers.py
+++ order/serializers.py
@@ -4,10 +4,16 @@ from order.models import Order, Payment, MenuItem
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    detail = serializers.CharField(help_text='Order detail')
+    clientName = serializers.CharField(help_text='Order client name')
+    price = serializers.FloatField(help_text='Order total price', read_only=True)
+    isPaid = serializers.BooleanField(help_text='Is order paid?', read_only=True)
+    isCancelled = serializers.BooleanField(help_text='Is order cancelled?', read_only=True)
+
     class Meta:
         model = Order
         fields = '__all__'
-        read_only_fields = ['price', 'isPaid']
+        read_only_fields = ['price', 'isPaid', 'isCancelled']
 
 
 class PaymentSerializer(serializers.ModelSerializer):
