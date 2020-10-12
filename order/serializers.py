bcf2dcdc7f53a36dedb2b687a613b2aecb6ea38e
Yifan Ai <ayfallen@hotmail.com>
2020-10-12 17:04:06 +1100

    Update serializer fields

--- order/serializers.py
+++ order/serializers.py
@@ -4,12 +4,12 @@ from order.models import Order, MenuItem
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    detail = serializers.CharField(initial='1 Latte with soy milk', default='1 Latte', help_text='Order detail')
-    clientName = serializers.CharField(initial='John Doe', default='John Doe', help_text='Order client name')
-    price = serializers.FloatField(initial='4', default='4', help_text='Order total price')
-    isPaid = serializers.BooleanField(initial=False, default=False, help_text='Is order paid?')
-    paymentDetail = serializers.CharField(initial='Cash', default='Cash', help_text='Order payment detail')
-    isCancelled = serializers.BooleanField(initial=False, default=False, help_text='Is order cancelled?')
+    detail = serializers.CharField(initial='1 Latte with soy milk', default='1 Latte', help_text='Order detail (coffee and additions)')
+    clientName = serializers.CharField(initial='John Doe', default='John Doe', help_text='Order customer name')
+    price = serializers.FloatField(initial='5', default='5', help_text='Order total price')
+    isPaid = serializers.BooleanField(initial=False, default=False, help_text='Has customer paid?')
+    paymentDetail = serializers.CharField(initial='Cash', default='Cash', help_text='Order payment detail (Payment method and change')
+    isCancelled = serializers.BooleanField(initial=False, default=False, help_text='Has order been cancelled?')
 
     class Meta:
         model = Order
@@ -17,8 +17,8 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(initial='Latte', default='Latte', help_text='Menu Item Name')
-    price = serializers.FloatField(initial='4', default='4', help_text='Menu Item price')
+    name = serializers.CharField(initial='Latte', default='Latte', help_text='Menu item name (coffee or addition)')
+    price = serializers.FloatField(initial='4', default='4', help_text='Menu item price')
 
     class Meta:
         model = MenuItem
