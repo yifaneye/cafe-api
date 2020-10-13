f3c34e800a4ee43a0c4db770636613fa80a66b45
Yifan Ai <ayfallen@hotmail.com>
2020-10-13 23:45:31 +1100

    Use online database

--- coolCoffee/settings.py
+++ coolCoffee/settings.py
@@ -79,9 +79,10 @@ WSGI_APPLICATION = 'coolCoffee.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django_s3_sqlite",
+        "NAME": "sqlite.db",
+        "BUCKET": "coffee-db",
     }
 }
 
